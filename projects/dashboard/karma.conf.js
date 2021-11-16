dbea82f83748241b67d2b4bdace81b51695f9cb0
Thiago Martins de Freitas <thiagodf@ciandt.com>
2021-11-16 14:50:09 -0300

    Movendo prettier para a raiz e padronizado os arquivos

--- projects/dashboard/karma.conf.js
+++ projects/dashboard/karma.conf.js
@@ -3,14 +3,14 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: "",
-    frameworks: ["jasmine", "@angular-devkit/build-angular"],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require("karma-jasmine"),
-      require("karma-chrome-launcher"),
-      require("karma-jasmine-html-reporter"),
-      require("karma-coverage"),
-      require("@angular-devkit/build-angular/plugins/karma"),
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       jasmine: {
@@ -25,9 +25,9 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "../../coverage/dashboard"),
-      subdir: ".",
-      reporters: [{ type: "html" }, { type: "text-summary" }],
+      dir: require('path').join(__dirname, '../../coverage/dashboard'),
+      subdir: '.',
+      reporters: [{ type: 'html' }, { type: 'text-summary' }],
     },
     thresholds: {
       emitWarning: false,
@@ -44,18 +44,18 @@ module.exports = function (config) {
         functions: 80,
       },
     },
-    reporters: ["progress", "coverage", "kjhtml"],
+    reporters: ['progress', 'coverage', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true,
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
-        base: "ChromeHeadless",
-        flags: ["--headless", "--no-sandbox", "--remote-debugging-port=9222"],
+        base: 'ChromeHeadless',
+        flags: ['--headless', '--no-sandbox', '--remote-debugging-port=9222'],
       },
     },
     browserDisconnectTolerance: 8,
