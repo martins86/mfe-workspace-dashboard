ef045fbc7ae067cec2141c0d3cb3a932dd97ec9c
Thiago Martins de Freitas <thiagodf@ciandt.com>
2021-11-17 16:50:04 -0300

    Correcões no coverage

--- projects/dashboard/karma.conf.js
+++ projects/dashboard/karma.conf.js
@@ -13,19 +13,14 @@ module.exports = function (config) {
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
-      jasmine: {
-        // you can add configuration options for Jasmine here
-        // the possible options are listed at https://jasmine.github.io/api/edge/Configuration.html
-        // for example, you can disable the random execution with `random: false`
-        // or set a specific seed with `seed: 4321`
-      },
+      jasmine: {},
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     jasmineHtmlReporter: {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/dashboard'),
+      dir: require('path').join(__dirname, '../../coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
@@ -33,6 +28,7 @@ module.exports = function (config) {
         { type: 'lcov', subdir: './' },
       ],
     },
+    reporters: ['progress', 'coverage', 'kjhtml'],
     thresholds: {
       emitWarning: false,
       global: {
@@ -48,7 +44,6 @@ module.exports = function (config) {
         functions: 80,
       },
     },
-    reporters: ['progress', 'coverage', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
